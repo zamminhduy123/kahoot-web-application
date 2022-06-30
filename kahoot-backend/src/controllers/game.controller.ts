170e59340ab1679fdc4720a5859b79eb22d45c0d
Võ Minh Lâm <64176716+Leo-TheCoder@users.noreply.github.com>
2022-06-30 21:51:21 +0700

    update: api create game

--- kahoot-backend/src/controllers/game.controller.ts
+++ kahoot-backend/src/controllers/game.controller.ts
@@ -1,23 +1,29 @@
 import GameModel from "../models/Game.model";
-import {Request, Response} from "express";
+import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 import { IUserRequest } from "../types";
 
 interface IGame {
-    owner: string;
-    game: {
-        question: string,
-        solution: string[],
-        answer: number,
-    }[]
+  owner: string;
+  game: {
+    question: string;
+    solution: string[];
+    answer: number;
+  }[];
 }
 
 export const createGame = async (req: IUserRequest, res: Response) => {
-    const owner = req.user?.userId;
-    const game = req.body as IGame;
-    res.json({
-        game,
-        owner,
-        accessToken: req.user?.accessToken,
-    });
-}
+  const owner = req.user?.userId;
+  const game = req.body as IGame;
+
+  await GameModel.create({
+    owner,
+    game,
+  });
+
+  res.json({
+    game,
+    owner,
+    accessToken: req.user?.accessToken,
+  });
+};
