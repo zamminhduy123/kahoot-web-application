41bd6c10d665f45306cf813b6afe3666046c7c04
Võ Minh Lâm <64176716+Leo-TheCoder@users.noreply.github.com>
2022-07-13 01:53:20 +0700

    update: endpoint get games

--- kahoot-backend/src/routes/game.router.ts
+++ kahoot-backend/src/routes/game.router.ts
@@ -2,10 +2,11 @@ import express from "express";
 import validateMdw from "../middlewares/validate.mdw";
 import { compulsoryAuth } from "../middlewares/authentication.mdw";
 import * as schemas from "../schemas";
-import { createGame } from "../controllers/game.controller";
+import { createGame, getYourGames } from "../controllers/game.controller";
 
 const router = express.Router();
 
 router.post("/", compulsoryAuth, validateMdw(schemas.gameSchema), createGame);
+router.get("/", compulsoryAuth, getYourGames);
 
 export default router;
