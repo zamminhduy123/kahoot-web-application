9b68400b19e44465219c8d970af92c79d3415fd5
Võ Minh Lâm <64176716+Leo-TheCoder@users.noreply.github.com>
2022-07-13 15:37:32 +0700

    add: function handle socket event

--- kahoot-backend/src/classes/Kahoot.class.ts
+++ kahoot-backend/src/classes/Kahoot.class.ts
@@ -8,24 +8,24 @@ interface ILiveGameKahoot extends IGameKahoot {
     first: {
       name: string;
       score: number;
-    },
+    };
     second: {
       name: string;
       score: number;
-    },
+    };
     third: {
       name: string;
       score: number;
-    },
+    };
     fourth: {
       name: string;
       score: number;
-    },
+    };
     fifth: {
       name: string;
       score: number;
-    }
-  }
+    };
+  };
 }
 
 export default class Kahoot {
@@ -97,18 +97,99 @@ export default class Kahoot {
   }
 
   getPlayer(playerId: string) {
-    return this.players.filter(player => player.playerId === playerId)[0];
+    return this.players.filter((player) => player.playerId === playerId)[0];
   }
 
   removePlayer(playerId: string) {
     const player = this.getPlayer(playerId);
-    if(player) {
-      this.players = this.players.filter(player => player.playerId !== playerId);
+    if (player) {
+      this.players = this.players.filter(
+        (player) => player.playerId !== playerId
+      );
     }
     return player;
   }
 
   getPlayersInRoom(hostId: string) {
-    return this.players.filter(player => player.hostId === hostId);
+    return this.players.filter((player) => player.hostId === hostId);
+  }
+
+  updateRankingBoard(hostId: string) {
+    const game = this.getGame(hostId);
+
+    if (!game.gameData.rankingBoard) {
+      game.gameData.rankingBoard = {
+        first: { name: "", score: 0 },
+        second: { name: "", score: 0 },
+        third: { name: "", score: 0 },
+        fourth: { name: "", score: 0 },
+        fifth: { name: "", score: 0 },
+      };
+    }
+    const rankingBoard = game.gameData.rankingBoard;
+
+    const playersInGame = this.getPlayersInRoom(hostId);
+
+    for (let i = 0; i < playersInGame.length; i++) {
+      if (playersInGame[i].gameData.score > rankingBoard.fifth.score) {
+        if (playersInGame[i].gameData.score > rankingBoard.fourth.score) {
+          if (playersInGame[i].gameData.score > rankingBoard.third.score) {
+            if (playersInGame[i].gameData.score > rankingBoard.second.score) {
+              if (playersInGame[i].gameData.score > rankingBoard.first.score) {
+                //First Place
+                rankingBoard.fifth.name = rankingBoard.fourth.name;
+                rankingBoard.fifth.score = rankingBoard.fourth.score;
+
+                rankingBoard.fourth.name = rankingBoard.third.name;
+                rankingBoard.fourth.score = rankingBoard.third.score;
+
+                rankingBoard.third.name = rankingBoard.second.name;
+                rankingBoard.third.score = rankingBoard.second.score;
+
+                rankingBoard.second.name = rankingBoard.first.name;
+                rankingBoard.second.score = rankingBoard.first.score;
+
+                rankingBoard.first.name = playersInGame[i].name;
+                rankingBoard.first.score = playersInGame[i].gameData.score;
+              } else {
+                //Second Place
+                rankingBoard.fifth.name = rankingBoard.fourth.name;
+                rankingBoard.fifth.score = rankingBoard.fourth.score;
+
+                rankingBoard.fourth.name = rankingBoard.third.name;
+                rankingBoard.fourth.score = rankingBoard.third.score;
+
+                rankingBoard.third.name = rankingBoard.second.name;
+                rankingBoard.third.score = rankingBoard.second.score;
+
+                rankingBoard.second.name = playersInGame[i].name;
+                rankingBoard.second.score = playersInGame[i].gameData.score;
+              }
+            } else {
+              //Third Place
+              rankingBoard.fifth.name = rankingBoard.fourth.name;
+              rankingBoard.fifth.score = rankingBoard.fourth.score;
+
+              rankingBoard.fourth.name = rankingBoard.third.name;
+              rankingBoard.fourth.score = rankingBoard.third.score;
+
+              rankingBoard.third.name = playersInGame[i].name;
+              rankingBoard.third.score = playersInGame[i].gameData.score;
+            }
+          } else {
+            //Fourth Place
+            rankingBoard.fifth.name = rankingBoard.fourth.name;
+            rankingBoard.fifth.score = rankingBoard.fourth.score;
+
+            rankingBoard.fourth.name = playersInGame[i].name;
+            rankingBoard.fourth.score = playersInGame[i].gameData.score;
+          }
+        } else {
+          //Fifth Place
+          rankingBoard.fifth.name = playersInGame[i].name;
+          rankingBoard.fifth.score = playersInGame[i].gameData.score;
+        }
+      }
+    }
   }
 }
--- kahoot-backend/src/controllers/socket.controller.ts
+++ kahoot-backend/src/controllers/socket.controller.ts
@@ -33,23 +33,205 @@ export default function (io: Server, socket: Socket, kahoot: Kahoot) {
     }
   };
 
-  const onHostJoinGame = function (payload: string) {};
+  const onHostJoinGame = async function (payload: { id: string }) {
+    const oldHostId = payload.id;
+    //Get game with old host id
+    const game = kahoot.getGame(oldHostId);
 
-  const onPlayerJoin = function (payload: string) {
-    console.log(payload);
+    if (game) {
+      //Changes the game host id to new host id
+      game.hostId = socket.id;
+
+      socket.join(game.pin);
+
+      //Gets players in game
+      const playerData = kahoot.getPlayersInRoom(oldHostId);
+      for (let i = 0; i < kahoot.players.length; i++) {
+        if (kahoot.players[i].hostId == oldHostId) {
+          kahoot.players[i].hostId = socket.id;
+        }
+      }
+
+      const gameId = game.gameData.gameId;
+      const _gameDoc = await GameModel.findById(gameId).lean();
+
+      //Get data of first question
+      const question = _gameDoc?.game[0].question;
+      const answers = _gameDoc?.game[0].solution;
+      const correctAnswer = _gameDoc?.game[0].answer;
+
+      socket.emit("gameQuestions", {
+        question,
+        answers,
+        correct: correctAnswer,
+        playersInGame: playerData.length,
+      });
+
+      io.to(game.pin).emit("gameStartedPlayer");
+      game.isLive = true;
+    } else {
+      socket.emit("noGameFound");
+    }
   };
 
-  const onPlayerJoinGame = function (payload: string) {};
+  const onPlayerJoin = function (payload: { pin: string; name: string }) {
+    const { pin, name } = payload;
+    let gameFound = false;
 
-  const onGameStart = function (payload: string) {};
+    for (let i = 0; i < kahoot.games.length; i++) {
+      const game = kahoot.games[i];
+      if (pin == game.pin) {
+        console.log("Player connected to game");
+        const hostId = game.hostId;
 
-  const onPlayerAnswer = function (payload: string) {};
+        //Add player to the game
+        kahoot.addPlayer(hostId, socket.id, name, game.gameData.gameId);
 
-  const onNextQuestion = function (payload: string) {};
+        //Player is joining room based on pin
+        socket.join(pin);
 
-  const onTimeOut = function (payload: string) {};
+        //Sending players data to display
+        const playersInGame = kahoot.getPlayersInRoom(hostId);
+        io.to(pin).emit("updatePlayerLobby", playersInGame);
+
+        gameFound = true; //Game has been found
+      }
+    }
+
+    if (!gameFound) {
+      //Player is sent back to 'join' page because game was not found with pin
+      socket.emit("noGameFound");
+    }
+  };
+
+  const onPlayerJoinGame = function (payload: { id: string }) {
+    const { id } = payload;
+    const player = kahoot.getPlayer(id);
+
+    if(player) {
+      const game = kahoot.getGame(player.hostId);
+      socket.join(game.pin);
+
+      // update player id with socket id
+      player.playerId = socket.id; 
+
+      const playerData = kahoot.getPlayersInRoom(game.hostId);
+      socket.emit('playerGameData', playerData);
+    } else {
+      //No player found
+      socket.emit('noGameFound');
+    }
+  };
+
+  const onGameStart = function () {
+    const game = kahoot.getGame(socket.id);
+    game.isLive = true;
+    //Tell player and host that game has started
+    socket.emit('gameStarted', game.hostId);
+  };
+
+  const onPlayerAnswer = function (payload: {
+    num: number,
+  }) {
+    const {num} = payload;
+    const player = kahoot.getPlayer(socket.id);
+    const hostId = player.hostId;
+    const playerNum = kahoot.getPlayersInRoom(hostId);
+    const game = kahoot.getGame(hostId);
+
+    if(game.isLive) {
+      player.gameData.answer = num;
+      game.gameData.playersAnswered += 1;
+      
+      const gameQuestion = game.gameData.question;
+      const correctAnswer = game.gameData.game[gameQuestion].answer;
+      
+      //Check player answer with correct answer
+      if(num == correctAnswer) {
+        player.gameData.score += 100;
+        io.to(game.pin).emit('getTime', socket.id);
+        socket.emit('answerResult', true);
+      }
+
+      //Check if all players answered
+      if(game.gameData.playersAnswered == playerNum.length) {
+        //Question has been ended since players all answered
+        game.isLive = false;
+        const playerData = kahoot.getPlayersInRoom(game.hostId);
+        io.to(game.pin).emit('questionOver', playerData, correctAnswer);
+      } else {
+        //update host screen of num players answered
+        io.to(game.pin).emit('updatePlayersAnswered', {
+          playersInGame: playerNum.length,
+          playersAnswerd: game.gameData.playersAnswered,
+        })
+      }
+    }
+  };
+
+  const onNextQuestion = function () {
+    const playerData = kahoot.getPlayersInRoom(socket.id);
+
+    //Reset players current answer to 0
+    for(let i = 0; i < kahoot.players.length; i++) {
+      if(kahoot.players[i].hostId == socket.id) {
+        kahoot.players[i].gameData.answer = 0;
+      }
+    }
+
+    const game = kahoot.getGame(socket.id);
+    game.gameData.playersAnswered = 0;
+    game.isLive = true;
+    game.gameData.question += 1;
+    
+    if(game.gameData.game.length >= game.gameData.question) {
+      let questionNum = game.gameData.question;
+      const question = game.gameData.game[questionNum].question;
+      const answers = game.gameData.game[questionNum].solution;
+      const correctAnswer = game.gameData.game[questionNum].answer;
+      
+      socket.emit('gameQuestions', {
+        question,
+        answers,
+        correct: correctAnswer,
+        playersInGame: playerData.length,
+      });
+    }
+    else {
+      kahoot.updateRankingBoard(game.hostId);
+      io.to(game.pin).emit('GameOver', game.gameData.rankingBoard);
+    }
+
+    io.to(game.pin).emit('nextQuestionPlayer');
+  };
+
+  const onTimeUp = function (payload: string) {
+    const game = kahoot.getGame(socket.id);
+    game.isLive = false;
+    const playerData = kahoot.getPlayersInRoom(game.hostId);
+    const gameQuestion = game.gameData.question;
+
+    const correctAnswer = game.gameData.game[gameQuestion].answer;
+    io.to(game.pin).emit('questionOver', playerData, correctAnswer);
+  };
 
   const onDisconnect = function (payload: string) {};
+
+  const onGetScore = function() {
+    const player = kahoot.getPlayer(socket.id);
+    socket.emit('newScore', player.gameData.score);
+  };
+
+  const onTime = function (payload: {
+    time: number,
+    player: string,
+  }) {
+    let time = payload.time / 20;
+    time = time * 100;
+    const playerId = payload.player;
+    const player = kahoot.getPlayer(playerId);
+    player.gameData.score += time;
+  };
 
   //When host connects for the first time
   socket.on("host-join", onHostJoin);
@@ -73,7 +255,10 @@ export default function (io: Server, socket: Socket, kahoot: Kahoot) {
   socket.on("next-question", onNextQuestion);
 
   //When time out for the question
-  socket.on("time-out", onTimeOut);
+  socket.on("time-up", onTimeUp);
+
+  socket.on('get-score', onGetScore);
+  socket.on('time', onTime);
 
   //When one socket disconnected
   socket.on("disconnect", onDisconnect);
