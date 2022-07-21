89d1a0559da5c731dd357ae605586d6d2b464be8
duy <ntminhduy123@gmail.com>
2022-07-22 00:20:23 +0700

    absolute image upload button

--- kahoot-frontend/src/pages/Creator/ImageInput.tsx
+++ kahoot-frontend/src/pages/Creator/ImageInput.tsx
@@ -64,8 +64,11 @@ const ImageInput: FunctionComponent<ImageInputProps> = (props) => {
 
 	return (
 		<>
-			<VStack gap="4">
+			<VStack gap="4" minH='250px' boxShadow='rgb(50 50 93 / 25%) 0px 2px 5px -1px, rgb(0 0 0 / 30%) 0px 1px 3px -1px' borderRadius='10px'>
 				<Button
+					position={'absolute'}
+					top='300'
+					bottom='0'
 					colorScheme={"teal"}
 					onClick={() => {
 						imageInput.current?.click()
