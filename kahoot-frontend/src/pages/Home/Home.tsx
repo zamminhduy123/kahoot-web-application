8d31615a648e36e0ac5d4a1f966342dcc2d116b5
Kim Nguyễn Duy Thiên <ngduytkim@gmail.com>
2022-07-22 01:01:34 +0700

    responsive home

--- kahoot-frontend/src/pages/Home/Home.tsx
+++ kahoot-frontend/src/pages/Home/Home.tsx
@@ -1,101 +1,101 @@
-import {
-  Box,
-  Button,
-  Center,
-  Flex,
-  HStack,
-  SlideFade,
-  VStack,
-} from "@chakra-ui/react";
+/** @format */
 
-import features from "../../assets/feature.png";
-import { Link } from "react-router-dom";
+import {
+	Box,
+	Button,
+	Center,
+	Flex,
+	HStack,
+	SlideFade,
+	Spacer,
+	Stack,
+	VStack,
+} from "@chakra-ui/react"
+
+import features from "../../assets/feature.png"
+import { Link } from "react-router-dom"
 
 const Achievement = (props: any) => {
-  return (
-    <Center
-      flex="1"
-      bgColor={props.color}
-      mr="4"
-      borderRadius={"10px"}
-      color="white"
-      fontWeight={600}
-      fontSize="xl"
-      boxShadow={
-        "rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px"
-      }
-      overflowWrap={'normal'}
-      textAlign={"center"}
-    >
-      {props.text}
-    </Center>
-  );
-};
+	return (
+		<Center
+			flex="1"
+			bgColor={props.color}
+			borderRadius={"10px"}
+			color="white"
+			fontWeight={600}
+			fontSize="lg"
+			boxShadow={
+				"rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px"
+			}
+			overflowWrap={"normal"}
+			textAlign={"center"}
+			p={{ base: "4", lg: "0" }}
+		>
+			{props.text}
+		</Center>
+	)
+}
 
 const Home = () => {
-  return (
-    <Box paddingTop='20px' flex={1} w='100%'>
-      <Flex direction={"column"} justify={"flex-start"} align="center">
-        <Box fontWeight={"900"} textAlign="center" fontSize={"3xl"}>
-          New Quiz Features
-        </Box>
-        <img src={features} />
+	return (
+		<VStack paddingTop="20px" w="100%">
+			<Box fontWeight={"900"} textAlign="center" fontSize={"3xl"}>
+				New Quiz Features
+			</Box>
+			<img src={features} />
 
-        <Button
-          width={"20%"}
-          padding="20px 10px"
-          fontWeight={"900"}
-          mb="4"
-          bgColor="green"
-          transition={"transform 0.2s ease-in-out"}
-          _hover={{ transform: "scale(1.05)" }}
-        >
-          <Link to="/join">Play now</Link>
-        </Button>
+			<Button
+				padding="20px 10px"
+				fontWeight={"900"}
+				mb="4"
+				bgColor="green"
+				transition={"transform 0.2s ease-in-out"}
+				_hover={{ transform: "scale(1.05)" }}
+			>
+				<Link to="/join">Play now</Link>
+			</Button>
 
-        <VStack
-          justify={"center"}
-          bgColor="#f2f2f2"
-          w={"100%"}
-          mt="2"
-          pt="2"
-          pb="8"
-        >
-          <Box
-            margin="0"
-            fontWeight={"900"}
-            textAlign="center"
-            fontSize={"3xl"}
-          >
-            <Box fontWeight={"500"} textAlign="center" fontSize={"xl"}>
-              More than
-            </Box>
-            100,000
-            <Box fontWeight={"500"} textAlign="center" fontSize={"xl"}>
-              players a year
-            </Box>
-          </Box>
-          <Flex w="60%" maxH="200px" minH='100px'>
-            <Achievement text="Easy to create!" color="red" />
-            <Achievement text="Easy to host!" color="blue" />
-            <Achievement text="Let make quiz more fun" color="orange" />
-            <Achievement text="And it's free" color="green" />
-          </Flex>
-          <Box h='10px'></Box>
-          <Button
-            width={"20%"}
-            padding="20px 10px"
-            fontWeight={"900"}
-            marginTop="10px"
-            transition={"transform 0.2s ease-in-out"}
-            _hover={{ transform: "scale(1.05)" }}
-          >
-            <Link to="/creator">Create your own quiz</Link>
-          </Button>
-        </VStack>
-      </Flex>
-    </Box>
-  );
-};
+			<VStack
+				justify={"center"}
+				bgColor="#f2f2f2"
+				w={"100%"}
+				mt="2"
+				pt="2"
+				pb="8"
+			>
+				<Box margin="0" fontWeight={"900"} textAlign="center" fontSize={"3xl"}>
+					<Box fontWeight={"500"} textAlign="center" fontSize={"xl"}>
+						More than
+					</Box>
+					100,000
+					<Box fontWeight={"500"} textAlign="center" fontSize={"xl"}>
+						players a year
+					</Box>
+				</Box>
+				<Stack
+					w="80%"
+					minH="100px"
+					direction={["column", "row"]}
+					spacing="24px"
+				>
+					<Achievement text="Easy to create!" color="red" />
+					<Achievement text="Easy to host!" color="blue" />
+					<Achievement text="Let make quiz more fun" color="orange" />
+					<Achievement text="And it's free" color="green" />
+				</Stack>
+				<Spacer />
+				<Button
+					padding="20px 10px"
+					fontWeight={"900"}
+					marginTop="10px"
+					transition={"transform 0.2s ease-in-out"}
+					_hover={{ transform: "scale(1.05)" }}
+				>
+					<Link to="/creator">Create your own quiz</Link>
+				</Button>
+			</VStack>
+		</VStack>
+	)
+}
 
-export default Home;
+export default Home
